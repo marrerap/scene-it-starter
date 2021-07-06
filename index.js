71799666b6f4908a50ea250d14454aaeb2971b09
marrerap <andrew.marrero@outlook.com>
2021-07-05 23:28:33 -0500

    added comments to better clarify js

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
 	// code here will execute after the document is loaded
-    
-
-    //     
-    // console.log(renderMovies(movieData))
-
 });
-// this function is outside of the document ready block.
-// it will take a database and map out each result
-// next it will then create an h2 element inside of a div with the movie title
-// then it will append the result as a string 
-// that will allow the ability to create and add this block of html
+
+
+
+
+// this next function will take a database and for each entry, create a div element
+// with a h2 element using key.title as its text. Then it will append this new
+// element to the movieHtmlArray variable as a string. we can then use the result
+// of this function to create title's for our search results.
+
+
+// this is the beginning of the function with an anonymous parameter
+// later we will add the databases we want mapped into the place of anonymous
+// parameter
 function renderMovies(movieArray) {
-    // use the map function to map a database and save it to a variable
+    // use the map function to map a database and run a function that will 
+    // create an element used to display the title of of the Movie
     const movieHtmlArray = movieArray.map((currentMovie) => {
-        // create and return movie title inside of an h2 and div element
+        // create a title inside of a div using the h2 and the title of the 
+        // movie from the results of the map function
         return `<div><h2>${currentMovie.Title}</h2></div><br>`
         
     })
-    // add the previous template literal to the variable above as a string
+    // add the previous expression to movieHtmlArray variable as a string.  
+    // this will allow us to call upon the code created in the previous step
+    // and utilize it in the html by adding titles 
     return movieHtmlArray.join('');
 }
 
+// this function will listen for a submission in the search bar that is inside
+// of a form. It will then display results on screen
+// first create a link for the form I want to listen to
 const myForm = document.getElementById('search-form');
+// create the listener using the link created in the previous step and listen
+// for a submit and then start a function
 myForm.addEventListener('submit', function(e){
-    // event listener code goes here
+    // prevent the default action of refreshing the page
     e.preventDefault();
-    const results = document.getElementById('movies-container').innerHTML = renderMovies(movieData);
+    // create a link to the element I want to add the results to and display the
+    // results
+    document.getElementById('movies-container').innerHTML = renderMovies(movieData);
     
 
 })
